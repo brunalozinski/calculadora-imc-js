20506095302e170ad99136a5829013d0777352e4
brunalozinski <lozinskibruna@gmail.com>
2021-08-10 11:29:53 -0300

    atualização do calculo

---
+++ script.js
@@ -0,0 +1,19 @@
+const altura = document.getElementById("altura")
+const peso = document.getElementById("peso")
+const calcular = document.getElementById("calcular")
+const resultado = document.getElementById("resultado")
+
+function calcularImc() {
+    const imc = parseFloat(peso.value) / parseFloat(altura.value ** 2)
+    resultado.textContent = imc
+}
+
+
+calcular.addEventListener("Click",calcularImc)
+
+
+
+
+
+
+
